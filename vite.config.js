9948343136d76250742600c16b79329aa0623621
DanyaLozhkin <delaynore@gmail.com>
2024-04-18 00:27:41 +0400

    Изменен конфиг

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/*.css',
-                'resources/js/*.js',
+                'resources/css/app.css',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
