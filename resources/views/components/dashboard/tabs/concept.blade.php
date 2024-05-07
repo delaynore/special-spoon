082b0a012c4ac2ac4595eca79fc329f5a11e62c8
DanyaLozhkin <delaynore@gmail.com>
2024-05-07 19:27:10 +0400

    Добавлен текстовый редактор при создании концепта с поддержкой вставки ссылок

--- resources/views/components/dashboard/tabs/concept.blade.php
+++ resources/views/components/dashboard/tabs/concept.blade.php
@@ -2,9 +2,9 @@
 
 <x-layout.dashboard :dictionary="$dictionary" :concepts="$concepts">
     @include('components.dashboard.tabs.tabs')
-    <div class="w-full">
+    <div class="w-full p-2.5">
         <p>{{ $concept->id }}</p>
         <h5 class="mb-2 text-2xl font-bold text-gray-900 dark:text-white text-wrap break-words">{{$concept->name}}</h5>
-        <p class="font-normal text-gray-700 dark:text-gray-400 text-wrap break-words">{!! $concept->definition !!}</p>
+        <div class="block w-full min-h-12 text-sm p-2.5 text-gray-900 bg-gray-50 rounded-lg border border-gray-300  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">{!! $concept->definition !!}</div>
     </div>
 </x-layout.dashboard>
--- resources/views/concept/create.blade.php
+++ resources/views/concept/create.blade.php
@@ -13,7 +13,7 @@
                         {{ __('Создание нового понятия') }}
                     </h3>
                     <a href="{{route('dictionary.show', $dictionary)}}" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" height="24" width="24">
                             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                         </svg>
                     </a>
@@ -30,17 +30,16 @@
                             <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Наименование" value="{{old('name')}}" required maxlength="50">
                             <x-input-error :messages="$errors->get('name')" class="mt-2" />
                         </div>
-                        <div class="sm:col-span-2">
-                            <label for="definition" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Определение')}}</label>
-                            <textarea id="definition" name="definition" value="{{old('definition')}}" rows="4" class="block p-2.5 w-full min-h-12 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="{{__('Определение понятия')}}" maxlength="1000" required></textarea>
+                        <div class="col-span-2">
+                            <x-head.text-editor :dictionary="$dictionary" label="{{ __('Определение')}}" id="definition" name="definition" textareaClass="block p-2.5 w-full min-h-12 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" :value="old('definition')"></x-head.text-editor>
                             <x-input-error :messages="$errors->get('definition')" class="mt-2" />
                         </div>
                     </div>
-                    <button type="submit" class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                        <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
-                        </svg>
+                    <button type="submit" class="w-fit text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm pl-4 pr-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                         {{ __('Создать') }}
+                        <svg class="w-4 h-4 ml-1 -mr-1 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z" clip-rule="evenodd" />
+                        </svg>
                     </button>
                 </form>
             </div>
