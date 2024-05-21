04bbc0a55e9e16e7b58ed29c8de0480448c7905b
DanyaLozhkin <delaynore@gmail.com>
2024-05-21 21:03:07 +0400

    Рефакторинг роутов

--- routes/web.php
+++ routes/web.php
@@ -10,32 +10,13 @@ use App\Http\Controllers\DictionaryController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RelationTypeController;
 use App\Http\Controllers\TagController;
-use App\Models\Concept;
-use App\Models\ConceptRelation;
-use App\Models\RelationType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('contact', 'components.pages.contact')->name('contact');
+Route::view('about', 'components.pages.about')->name('about');
 
-Route::get("contact", function () {
-    return view("components.pages.contact");
-})->name('contact');
-
-Route::get("/about", function () {
-    return view("components.pages.about");
-})->name('about');
-
-Route::redirect("/home", "/");
+Route::redirect('/home', '/');
 
 Route::get('/', function (Request $request) {
     if (request('search')) {
@@ -47,81 +28,57 @@ Route::get('/', function (Request $request) {
     return view('components.pages.home', compact('dictionaries'));
 })->name('home');
 
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::resource('/my', DictionaryController::class)
-    ->middleware(['auth', 'verified'])
-    ->names([
-        'index' => 'my',
-        'create' => 'dictionary.create',
-        'store' => 'dictionary.store',
-        'edit' => 'dictionary.edit',
-        'destroy' => 'dictionary.destroy',
-        'update' => 'dictionary.update',
-        'show' => 'dictionary.show',
-    ]);
-
-Route::get('/my/{dictionary}/dashboard', [DictionaryController::class, 'show'])
-    ->name('dictionary.show')
-    ->middleware(['auth', 'verified']);
-
-Route::resource('/my/{dictionary}/concept/', ConceptController::class)
-    ->middleware(['auth', 'verified'])
-    ->names([
-        'create' => 'concept.create',
-        'destroy' => 'concept.destroy',
-        'store' => 'concept.store',
-    ]);
-Route::put('/my/{dictionary}/concept/{concept}', [ConceptController::class, 'update'])
-    ->name('concept.update')
-    ->middleware(['auth', 'verified']);
-Route::get('/my/{dictionary}/concept/{concept}', [ConceptController::class, 'edit'])
-    ->name('concept.edit')
-    ->middleware(['auth', 'verified']);
-
-Route::get('/my/{dictionary}/dashboard/{concept}/', [ConceptController::class, 'show'])
-    ->name('concept.show')
-    ->middleware(['auth', 'verified']);
-
-Route::delete('/my/{dictionary}/concept/{concept}', [ConceptController::class, 'destroy'])
-    ->middleware(['auth', 'verified']);
-
-Route::get('/my/{dictionary}/examples/{concept}', [ConceptController::class, 'examples'])->name('concept.examples')
-    ->middleware(['auth', 'verified']);
-    Route::get('/my/{dictionary}/attachments/{concept}', [ConceptController::class, 'attachments'])->name('concept.attachments')
-    ->middleware(['auth', 'verified']);
-
-// Attachments for concept
-Route::post('/my/{dictionary}/attachments/{concept}', [AttachmentController::class, 'store'])->name('attachment.store')
-->middleware(['auth', 'verified']);
-Route::get('/my/{dictionary}/attachments/{concept}/create', [AttachmentController::class, 'create'])->name('attachment.create');
-Route::delete('/my/{dictionary}/attachments/{concept}/{attachment}', [AttachmentController::class, 'destroy'])->name('attachment.destroy');
-
-Route::get('/tags', [TagController::class, 'index']);
-
-
-Route::prefix('my/{dictionary}')->group(function () {
-    Route::get('/export', [DictionaryController::class, 'export'])->name('dictionary.export');
-
-    Route::resource('concept/{concept}/attributes', ConceptAttributeController::class)->only(['create', 'store', 'destroy'])
-        ->names([
-            'create' => 'concept.attribute.create',
-            'store' => 'concept.attribute.store',
-            'destroy' => 'concept.attribute.destroy',
+    Route::middleware(['verified'])->group(function () {
+        Route::resource('my', DictionaryController::class)->names([
+            'index' => 'my',
+            'create' => 'dictionary.create',
+            'store' => 'dictionary.store',
+            'edit' => 'dictionary.edit',
+            'destroy' => 'dictionary.destroy',
+            'update' => 'dictionary.update',
+            'show' => 'dictionary.show',
         ]);
-});
 
-Route::resource('attributes', AttributeController::class)
-    ->names([
+        Route::get('my/{dictionary}/dashboard', [DictionaryController::class, 'show'])->name('dictionary.show');
+
+        Route::prefix('my/{dictionary}')->group(function () {
+            Route::resource('concept', ConceptController::class)->only(['create', 'store', 'destroy'])->names([
+                'create' => 'concept.create',
+                'store' => 'concept.store',
+                'destroy' => 'concept.destroy',
+            ]);
+            Route::put('concept/{concept}', [ConceptController::class, 'update'])->name('concept.update');
+            Route::get('concept/{concept}', [ConceptController::class, 'edit'])->name('concept.edit');
+            Route::get('dashboard/{concept}', [ConceptController::class, 'show'])->name('concept.show');
+            Route::delete('concept/{concept}', [ConceptController::class, 'destroy'])->name('concept.destroy');
+            Route::get('examples/{concept}', [ConceptController::class, 'examples'])->name('concept.examples');
+            Route::get('attachments/{concept}', [ConceptController::class, 'attachments'])->name('concept.attachments');
+
+            // Attachments for concept
+            Route::post('attachments/{concept}', [AttachmentController::class, 'store'])->name('attachment.store');
+            Route::get('attachments/{concept}/create', [AttachmentController::class, 'create'])->name('attachment.create');
+            Route::delete('attachments/{concept}/{attachment}', [AttachmentController::class, 'destroy'])->name('attachment.destroy');
+
+            Route::get('export', [DictionaryController::class, 'export'])->name('dictionary.export');
+
+            Route::resource('concept/{concept}/attributes', ConceptAttributeController::class)->only(['create', 'store', 'destroy'])->names([
+                'create' => 'concept.attribute.create',
+                'store' => 'concept.attribute.store',
+                'destroy' => 'concept.attribute.destroy',
+            ]);
+            Route::get('concept/{concept}/relation/create', [ConceptRelationController::class, 'create'])->name('concept.relation.create');
+            Route::post('concept/{concept}/relation', [ConceptRelationController::class, 'store'])->name('concept.relation.store');
+            Route::delete('concept/{concept}/relation/{relation}', [ConceptRelationController::class, 'destroy'])->name('concept.relation.destroy');
+            Route::put('concept/{concept}/relation/{relation}', [ConceptRelationController::class, 'update'])->name('concept.relation.update');
+        });
+    });
+
+    Route::resource('attributes', AttributeController::class)->names([
         'create' => 'attribute.create',
         'store' => 'attribute.store',
         'destroy' => 'attribute.destroy',
@@ -129,44 +86,37 @@ Route::resource('attributes', AttributeController::class)
         'edit' => 'attribute.edit',
         'show' => 'attribute.show',
         'index' => 'attribute.index',
-    ])
-    ->middleware(['auth', 'verified']);
+    ]);
 
-Route::delete('concept/{concept}/examples/{exampleNumber}', [ConceptAttributeValueController::class, 'destroy'])
-    ->name('concept.example.destroy')->middleware(['auth', 'verified']);
-Route::get('concept/{concept}/examples/{exampleNumber}', [ConceptAttributeValueController::class, 'edit'])
-    ->name('concept.example.edit')->middleware(['auth', 'verified']);
-Route::put('concept/{concept}/examples/{exampleNumber}', [ConceptAttributeValueController::class, 'edit'])
-    ->name('concept.example.update')->middleware(['auth', 'verified']);
+    Route::delete('concept/{concept}/examples/{exampleNumber}', [ConceptAttributeValueController::class, 'destroy'])->name('concept.example.destroy');
+    Route::get('concept/{concept}/examples/{exampleNumber}', [ConceptAttributeValueController::class, 'edit'])->name('concept.example.edit');
+    Route::put('concept/{concept}/examples/{exampleNumber}', [ConceptAttributeValueController::class, 'edit'])->name('concept.example.update');
 
-Route::resource('concept/{concept}/example', ConceptAttributeValueController::class)->only(['store', 'update', 'create'])->middleware(['auth', 'verified'])
-    ->names([
+    Route::resource('concept/{concept}/example', ConceptAttributeValueController::class)->only(['store', 'update', 'create'])->names([
         'create' => 'concept.example.create',
         'store' => 'concept.example.store',
         'update' => 'concept.example.update',
         'edit' => 'concept.example.edit',
     ]);
 
-Route::resource('tags', TagController::class)
-    ->names([
+    Route::resource('tags', TagController::class)->names([
         'create' => 'tag.create',
         'store' => 'tag.store',
         'destroy' => 'tag.destroy',
         'update' => 'tag.update',
         'edit' => 'tag.edit',
         'index' => 'tag.index',
-    ])
-    ->middleware(['auth', 'verified']);
+    ]);
 
-Route::resource('relations', RelationTypeController::class)->names([
-    'create' => 'relation-type.create',
-    'store' => 'relation-type.store',
-    'destroy' => 'relation-type.destroy',
-    'update'=> 'relation-type.update',
-    'edit'=> 'relation-type.edit',
-    'index' => 'relation-type.index',
-])->middleware('auth');
-
+    Route::resource('relations', RelationTypeController::class)->names([
+        'create' => 'relation-type.create',
+        'store' => 'relation-type.store',
+        'destroy' => 'relation-type.destroy',
+        'update' => 'relation-type.update',
+        'edit' => 'relation-type.edit',
+        'index' => 'relation-type.index',
+    ]);
+});
 
 Route::fallback(function () {
     return view('errors.404');
