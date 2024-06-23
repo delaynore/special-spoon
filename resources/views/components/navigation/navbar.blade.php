83fd232b5fa67331a5143de782dcf5f4ed93c013
Daniil Lozhkin <delaynore@gmail.com>
2024-06-23 23:43:26 +0400

    config

--- resources/views/components/navigation/navbar.blade.php
+++ resources/views/components/navigation/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
         <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
             <x-application-logo class="block w-auto text-gray-800 fill-current h-9 dark:text-gray-200" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ env('APP_NAME') }}</span>
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ config('app.name')}}</span>
         </a>
         <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
             <span class="sr-only">{{ __('screen-reader.modal.open') }}</span>
--- resources/views/components/shared/logo-title.blade.php
+++ resources/views/components/shared/logo-title.blade.php
@@ -4,5 +4,5 @@
 
 <a href="{{ $href }}" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
     <x-application-logo class="w-8 h-8 mr-3 fill-current" />
-    {{ env('APP_NAME') }}
+    {{ config('app.name') }}
 </a>
