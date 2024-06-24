0bc2f46549baf8a955d05319698319762ee17079
Daniil Lozhkin <delaynore@gmail.com>
2024-06-24 16:12:32 +0400

    fix bug on update concept

--- app/Rules/UniqueWithoutCaseConcepts.php
+++ app/Rules/UniqueWithoutCaseConcepts.php
@@ -11,15 +11,22 @@ class UniqueWithoutCaseConcepts implements ValidationRule
 {
     protected string $column;
     protected array $where;
+    protected array $whereNot;
 
-    public function __construct(array $where = [])
+    public function __construct(array $where = [], array $whereNot = [])
     {
         $this->where = $where;
+        $this->whereNot = $whereNot;
     }
 
     public function validate(string $attribute, mixed $value, Closure $fail) : void
     {
         $query = Concept::where('name', 'ilike', $value);
+
+        foreach ($this->whereNot as $column => $value) {
+            $query->whereNot($column, $value);
+        }
+
         foreach ($this->where as $column => $value) {
             $query->where($column, $value);
         }
